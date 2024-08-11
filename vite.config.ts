c961dd7597d68e3ca7416369bbd1b405bbc7e008
AndrijKolomijec <akolomijec@purple-technology.com>
2024-08-11 22:18:05 +0200

    Edited vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			input: {
+				background: resolve(__dirname, 'src/background.ts'),
+				changeCredsContent: resolve(__dirname, 'src/changeCredsContent.ts'),
+				refreshRateExceededContent: resolve(
+					__dirname,
+					'src/refreshRateExceededContent.ts'
+				),
+				loadMoreContent: resolve(__dirname, 'src/loadMoreContent.ts'),
+				popup: resolve(__dirname, 'index.html')
+			},
+			output: {
+				entryFileNames: '[name].js',
+				chunkFileNames: 'chunks/[name].js',
+				assetFileNames: 'assets/[name].[ext]'
+			}
+		}
+	}
 })
